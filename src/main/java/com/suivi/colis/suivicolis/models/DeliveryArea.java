a9b29681180d938c8e090a6ce5370f24394e3206
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-04-23 19:56:47 +0100

    adding DeliveryArea Entity

---
+++ src/main/java/com/suivi/colis/suivicolis/models/DeliveryArea.java
@@ -0,0 +1,45 @@
+/*
+ * **
+ *  * @project : SuiviColis
+ *  * @author : Abdelhak Zaaim
+ *  * @email : abdelhakzammii@gmail.com
+ *  * @created : 23/04/2024, 19:44
+ *  * @modified : 23/04/2024, 19:44
+ *  * @description : This file is part of the SuiviColis project.
+ *  * @license : MIT License
+ *  *
+ *  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *  *
+ *  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *  **
+ */
+
+package com.suivi.colis.suivicolis.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class DeliveryArea {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String areaName;
+
+    @Column(unique = true)
+    private String areaCode;
+
+    // Add other fields as needed
+}
