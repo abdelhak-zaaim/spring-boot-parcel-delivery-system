41e076f99f4a4547b876eddce76bc241c9153e6f
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-04-26 17:26:51 +0100

    Improve Exception Handling Mechanism

--- src/main/java/com/suivi/colis/suivicolis/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/suivi/colis/suivicolis/exceptions/GlobalExceptionHandler.java
@@ -15,6 +15,7 @@ package com.suivi.colis.suivicolis.exceptions;
 import com.suivi.colis.suivicolis.exceptions.customexptions.IllegalUserAttributesException;
 import com.suivi.colis.suivicolis.exceptions.models.Error;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.ObjectError;
@@ -28,13 +29,13 @@ import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
+
+
+
+
+@Order(2)
 @ControllerAdvice
 public class GlobalExceptionHandler {
-    private final View error;
-
-    public GlobalExceptionHandler(View error) {
-        this.error = error;
-    }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<List<String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
@@ -47,12 +48,6 @@ public class GlobalExceptionHandler {
     }
 
 
-    @ExceptionHandler(IllegalUserAttributesException.class)
-    public ResponseEntity<?> handleInvalidUserAttributesException(IllegalUserAttributesException ex, HttpServletRequest request) {
-        Error error = new Error(ex.getMessage(), HttpStatus.BAD_REQUEST.value(), new Date(), request.getRequestURI());
-
-        return createErrorResponse(error, HttpStatus.BAD_REQUEST, request.getHeader("Accept"));
-    }
 
 
     @ExceptionHandler(RuntimeException.class)
@@ -62,16 +57,4 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(errors, HttpStatus.INTERNAL_SERVER_ERROR );
     }
 
-
-    // FIXME: this methos should be enhanced , actually it's not working as expected
-    // todo : this need tension ,
-    private ResponseEntity<?> createErrorResponse(Object errorBody, HttpStatus status, String acceptHeader) {
-        if (acceptHeader != null && acceptHeader.contains("application/json")) {
-            return new ResponseEntity<>(errorBody, status);
-        } else {
-            ModelAndView modelAndView = new ModelAndView("error");
-            modelAndView.addObject("error", errorBody);
-            return new ResponseEntity<>(modelAndView, status);
-        }
-    }
 }
---
+++ src/main/java/com/suivi/colis/suivicolis/exceptions/SpecificExceptionHandler.java
@@ -0,0 +1,35 @@
+/*
+ * **
+ *  * @project : SuiviColis
+ *  * @created : 26/04/2024, 17:21
+ *  * @modified : 26/04/2024, 17:21
+ *  * @description : This file is part of the SuiviColis project.
+ *  * @license : MIT License
+ * **
+ */
+
+package com.suivi.colis.suivicolis.exceptions;
+
+
+import com.suivi.colis.suivicolis.exceptions.customexptions.IllegalUserAttributesException;
+import com.suivi.colis.suivicolis.exceptions.models.Error;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Date;
+
+@Order(1)
+@ControllerAdvice
+public class SpecificExceptionHandler {
+    @ExceptionHandler(IllegalUserAttributesException.class)
+    public ResponseEntity<?> handleInvalidUserAttributesException(IllegalUserAttributesException ex, HttpServletRequest request) {
+        Error error = new Error(ex.getMessage(), HttpStatus.BAD_REQUEST.value(), new Date(), request.getRequestURI());
+
+        return BaseExceptionHandler.createErrorResponse(error, HttpStatus.BAD_REQUEST, request.getHeader("Accept"));
+    }
+
+}
