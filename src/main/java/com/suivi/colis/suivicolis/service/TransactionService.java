7732498556931050822fdd25cc1e4824daf52c9c
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-05-01 19:10:27 +0100

    add TransactionService interface

---
+++ src/main/java/com/suivi/colis/suivicolis/service/TransactionService.java
@@ -0,0 +1,22 @@
+/*
+ * **
+ *  * @project : SuiviColis
+ *  * @created : 01/05/2024, 18:56
+ *  * @modified : 01/05/2024, 18:56
+ *  * @description : This file is part of the SuiviColis project.
+ *  * @license : MIT License
+ * **
+ */
+
+package com.suivi.colis.suivicolis.service;
+
+import com.suivi.colis.suivicolis.entity.Transaction;
+
+import java.util.List;
+
+public interface TransactionService {
+    void deleteTransaction(Long id);
+    Transaction loadTransactionById(Long id);
+    Transaction saveTransaction(Transaction transaction);
+    List<Transaction> makeMultipleTransactions(List<Transaction> transactions);
+}
