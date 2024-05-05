9f5d70417d5debd6d1d0bf568d13ad182e70a3b6
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-05-05 13:44:17 +0100

    Refactor AgencyRequestDto to validate and format phone numbers

--- src/main/java/com/suivi/colis/delix/dto/request/AgencyRequestDto.java
+++ src/main/java/com/suivi/colis/delix/dto/request/AgencyRequestDto.java
@@ -12,14 +12,14 @@
 package com.suivi.colis.delix.dto.request;
 
 import com.suivi.colis.delix.util.Constants;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Value;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
+
 import java.io.Serializable;
-import java.util.Date;
-import java.util.Set;
+import java.util.regex.Matcher;
+
 
 /**
  * DTO for {@link com.suivi.colis.delix.entity.Agency}
@@ -27,7 +27,6 @@ import java.util.Set;
 @Value
 public class AgencyRequestDto implements Serializable {
     Long id;
-    @NotBlank
     @NotNull
     String agencyName;
     @NotNull
@@ -39,4 +38,44 @@ public class AgencyRequestDto implements Serializable {
     String agencyEmail;
     @NotNull
     MapsLocationPointRequestDto locationPoint;
+
+    public AgencyRequestDto(Long id, @NotNull String agencyName, @NotNull AddressRequestDto agencyAddress,
+                            @NotNull String agencyContactNumber, @NotNull String agencyEmail,
+                            @NotNull MapsLocationPointRequestDto locationPoint) {
+        this.id = id;
+        this.agencyName = agencyName;
+        this.agencyAddress = agencyAddress;
+
+        this.agencyContactNumber = validatePhoneNumber(convertPhoneNumber(agencyContactNumber));
+        this.agencyEmail = agencyEmail;
+        this.locationPoint = locationPoint;
+    }
+
+    private String convertPhoneNumber(String phoneNumber) {
+        if (phoneNumber.startsWith("0")) {
+            phoneNumber = Constants.COUNTRY_CODE + phoneNumber.substring(1);
+        } else if (!phoneNumber.startsWith(Constants.COUNTRY_CODE)) {
+            phoneNumber = Constants.COUNTRY_CODE + phoneNumber;
+        }
+
+        return phoneNumber;
+    }
+
+    private String validatePhoneNumber(String phoneNumber) {
+        if (phoneNumber == null || phoneNumber.isEmpty()) {
+            throw new IllegalArgumentException("Phone number cannot be null or empty");
+        }
+
+        String pattern = Constants.MOROCCAN_NUMBER_REGEXP;
+        java.util.regex.Pattern pattern1 = java.util.regex.Pattern.compile(pattern);
+        java.util.regex.Matcher matcher = pattern1.matcher(phoneNumber);
+
+        if (!matcher.find()) {
+            throw new IllegalArgumentException("Invalid phone number");
+        } else {
+            return phoneNumber;
+        }
+    }
+
+
 }
