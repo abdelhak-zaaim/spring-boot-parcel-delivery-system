7e21cb5f36936965ec435251f15caab507e40170
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-05-07 19:58:14 +0100

    security enhanceement , bugs fixing

--- src/main/java/com/suivi/colis/delix/service/Impl/TransactionServiceImpl.java
+++ src/main/java/com/suivi/colis/delix/service/Impl/TransactionServiceImpl.java
@@ -12,7 +12,10 @@
 package com.suivi.colis.delix.service.Impl;
 
 import com.suivi.colis.delix.entity.Transaction;
+import com.suivi.colis.delix.entity.User;
+import com.suivi.colis.delix.model.enums.TransactionType;
 import com.suivi.colis.delix.repository.TransactionRepo;
+import com.suivi.colis.delix.repository.UserRepo;
 import com.suivi.colis.delix.service.TransactionService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
@@ -24,20 +27,17 @@ import java.util.List;
 public class TransactionServiceImpl implements TransactionService {
     UserServiceImpl userService;
     TransactionRepo transactionRepo;
+    private UserRepo userRepository;
 
-    public TransactionServiceImpl(TransactionRepo transactionRepo, UserServiceImpl userService) {
+    public TransactionServiceImpl(TransactionRepo transactionRepo, UserServiceImpl userService, UserRepo userRepository) {
         this.transactionRepo = transactionRepo;
         this.userService = userService;
+        this.userRepository = userRepository;
     }
 
 
     @Override
-    public void deleteTransaction(Long id) {
-        transactionRepo.deleteById(id);
-
-    }
-
-    @Override
+    @Transactional(readOnly = true)
     public Transaction loadTransactionById(Long id) {
         return transactionRepo.findById(id).orElse(null);
     }
@@ -54,4 +54,25 @@ public class TransactionServiceImpl implements TransactionService {
     public List<Transaction> makeMultipleTransactions(List<Transaction> transactions) {
         return transactionRepo.saveAll(transactions);
     }
+
+
+    @Transactional(isolation = Isolation.SERIALIZABLE)
+    public Transaction transfer(User fromUser, User toUser, double amount, String description, TransactionType transactionType) {
+
+        fromUser.setBalance(fromUser.getBalance() - amount);
+        userRepository.save(fromUser);
+
+        toUser.setBalance(toUser.getBalance() + amount);
+        userRepository.save(toUser);
+
+        Transaction transaction = new Transaction();
+        transaction.setFromUser(fromUser);
+        transaction.setToUser(toUser);
+        transaction.setAmount(amount);
+        transaction.setTransactionType(transactionType);
+        transaction.setDescription(description);
+
+        return transactionRepo.save(transaction);
+    }
+
 }
