06109cec5ab47ea122fe1fde5f016720365abd16
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-05-08 18:16:50 +0100

    spring security enhancement and bugs fixing

--- src/main/java/com/suivi/colis/delix/securingweb/SecurityConfig.java
+++ src/main/java/com/suivi/colis/delix/securingweb/SecurityConfig.java
@@ -10,6 +10,7 @@
 
 package com.suivi.colis.delix.securingweb;
 
+import org.springframework.security.config.annotation.AbstractConfiguredSecurityBuilder;
 import com.suivi.colis.delix.model.enums.Role;
 import com.suivi.colis.delix.service.Impl.UserServiceImpl;
 import org.springframework.context.annotation.Bean;
@@ -21,8 +22,11 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
@@ -31,107 +35,113 @@ import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 @Configuration
 public class SecurityConfig {
 
-   @Configuration
-   @Order(1)
-   public static class AdminSecurityConfig {
+//   @Bean
+//   public UserDetailsService userDetailsService() {
+//      InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+//      manager.createUser(User.withUsername("user").password(passwordEncoder().encode("userPass")).roles("USER").build());
+//      manager.createUser(User.withUsername("admin").password(passwordEncoder().encode("adminPass")).roles("ADMIN").build());
+//      return manager;
+//   }
 
-      @Bean
-      public SecurityFilterChain securityFilterChainAdmin(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
-         MvcRequestMatcher.Builder mvcMatcherBuilder = new MvcRequestMatcher.Builder(introspector);
-         http.securityMatcher("/admin*")
-                 .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
-                         authorizationManagerRequestMatcherRegistry.requestMatchers(mvcMatcherBuilder.pattern("/admin*")).hasRole("ADMIN"))
+    @Bean
+    public AuthenticationManager authenticationManagerAdmin(UserServiceImpl userService, PasswordEncoder passwordEncoder) {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder);
 
-
-                 // log in
-                 .formLogin(httpSecurityFormLoginConfigurer ->
-                         httpSecurityFormLoginConfigurer.loginPage("/admin/login")
-                                 .loginProcessingUrl("/admin/login")
-                                 .failureUrl("/admin/login?error=loginError")
-                                 .defaultSuccessUrl("/admin/"))
-                 // logout
-                 .logout(httpSecurityLogoutConfigurer ->
-                         httpSecurityLogoutConfigurer.logoutUrl("/admin/logout")
-                                 .logoutSuccessUrl("/protectedLinks")
-                                 .deleteCookies("JSESSIONID"))
-                 .exceptionHandling(httpSecurityExceptionHandlingConfigurer ->
-                         httpSecurityExceptionHandlingConfigurer.accessDeniedPage("/403"))
-                 ;
-
-         return http.build();
-      }
-      @Bean
-      public PasswordEncoder passwordEncoderAmin() {
-         return new BCryptPasswordEncoder();
-      }
-
-      @Bean
-      public AuthenticationManager authenticationManagerAdmin(
-              UserServiceImpl userService,
-              PasswordEncoder passwordEncoder) {
-         DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-         authenticationProvider.setUserDetailsService(userService);
-         authenticationProvider.setPasswordEncoder(passwordEncoder);
-
-         return new ProviderManager(authenticationProvider);
-      }
+        return new ProviderManager(authenticationProvider);
+    }
 
 
-   }
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 
-   @Configuration
-   @Order(2)
-   public static class UserSecurityConfig {
+    @Configuration
+    @Order(1)
+    public static class AdminSecurityConfig {
 
-      @Bean
-      public SecurityFilterChain filterChainApp2(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
-         MvcRequestMatcher.Builder mvcMatcherBuilder = new MvcRequestMatcher.Builder(introspector);
-         http.securityMatcher("/*")
-                 .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
-                         authorizationManagerRequestMatcherRegistry.requestMatchers(mvcMatcherBuilder.pattern("/user*")).hasRole("USER"))
-                 .securityMatcher("/").authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
-                         authorizationManagerRequestMatcherRegistry.requestMatchers(mvcMatcherBuilder.pattern("/")).permitAll())
-                 // log in
-                 .formLogin(httpSecurityFormLoginConfigurer ->
-                         httpSecurityFormLoginConfigurer.loginPage("/login")
-                                 .loginProcessingUrl("/login")
-                                 .failureUrl("/login?error=loginError")
-                                 .defaultSuccessUrl("/"))
-                 // logout
-                 .logout(httpSecurityLogoutConfigurer ->
-                         httpSecurityLogoutConfigurer.logoutUrl("/logout")
-                                 .logoutSuccessUrl("/protectedLinks")
-                                 .deleteCookies("JSESSIONID"))
-                 .exceptionHandling(httpSecurityExceptionHandlingConfigurer ->
-                         httpSecurityExceptionHandlingConfigurer.accessDeniedPage("/403"))
-                 ;
-         return http.build();
-      }
-      @Bean
-      public PasswordEncoder passwordEncoder() {
-         return new BCryptPasswordEncoder();
-      }
-
-      @Bean
-      public AuthenticationManager authenticationManagerUser(
-              UserServiceImpl userService,
-              PasswordEncoder passwordEncoder) {
-         DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-         authenticationProvider.setUserDetailsService(userService);
-         authenticationProvider.setPasswordEncoder(passwordEncoder);
-
-         return new ProviderManager(authenticationProvider);
-      }
+        @Bean
+        public SecurityFilterChain securityFilterChainAdmin(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
+            MvcRequestMatcher.Builder mvcMatcherBuilder = new MvcRequestMatcher.Builder(introspector);
+            http.securityMatcher("/admin*").authorizeHttpRequests(
+                            authorizationManagerRequestMatcherRegistry ->
+                            {
+                                authorizationManagerRequestMatcherRegistry.requestMatchers(mvcMatcherBuilder.pattern("/admin*")).hasRole("ADMIN");
+                                authorizationManagerRequestMatcherRegistry.requestMatchers(mvcMatcherBuilder.pattern("/admin/login")).permitAll();
+                            }
 
 
-   }
+                    ).securityMatcher("/admin*","/admin/**")
+
+
+                    .formLogin(httpSecurityFormLoginConfigurer -> httpSecurityFormLoginConfigurer.loginPage("/admin/login").loginProcessingUrl("/admin/login").failureUrl("/admin/login?error=loginError").defaultSuccessUrl("/admin/"))
+
+                    .logout(httpSecurityLogoutConfigurer -> httpSecurityLogoutConfigurer.logoutUrl("/admin/logout").logoutSuccessUrl("/protectedLinks").deleteCookies("JSESSIONID")).exceptionHandling(httpSecurityExceptionHandlingConfigurer -> httpSecurityExceptionHandlingConfigurer.accessDeniedPage("/403"))
+
+
+            ;
+
+            return http.build();
+        }
+
+
+//      @Bean
+//      public AuthenticationManager authenticationManagerAdmin(
+//              UserServiceImpl userService,
+//              PasswordEncoder passwordEncoder) {
+//         DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+//         authenticationProvider.setUserDetailsService(userService);
+//         authenticationProvider.setPasswordEncoder(passwordEncoder);
+//
+//         return new ProviderManager(authenticationProvider);
+//      }
+
+
+    }
+
+
+    @Configuration
+    @Order(2)
+    public static class UserSecurityConfig {
+
+        @Bean
+        public SecurityFilterChain filterChainApp2(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
+            MvcRequestMatcher.Builder mvcMatcherBuilder = new MvcRequestMatcher.Builder(introspector);
+            http.securityMatcher("/*").authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry.requestMatchers(mvcMatcherBuilder.pattern("/user*")).hasRole("USER")).securityMatcher("/*")
 
 
 
+                    .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> {
+                                authorizationManagerRequestMatcherRegistry.requestMatchers(mvcMatcherBuilder.pattern("/")).permitAll();
+                                authorizationManagerRequestMatcherRegistry.requestMatchers(mvcMatcherBuilder.pattern("/login*")).permitAll();
+                            }
+
+                    )
+
+                    // log in
+                    .formLogin(httpSecurityFormLoginConfigurer -> httpSecurityFormLoginConfigurer.loginPage("/login").loginProcessingUrl("/login").failureUrl("/login?error=loginError").defaultSuccessUrl("/"))
+                    // logout
+                    .logout(httpSecurityLogoutConfigurer -> httpSecurityLogoutConfigurer.logoutUrl("/logout").logoutSuccessUrl("/protectedLinks").deleteCookies("JSESSIONID")).exceptionHandling(httpSecurityExceptionHandlingConfigurer -> httpSecurityExceptionHandlingConfigurer.accessDeniedPage("/403"));
+            return http.build();
+        }
 
 
+//      @Bean
+//      public AuthenticationManager authenticationManagerUser(
+//              UserServiceImpl userService,
+//              PasswordEncoder passwordEncoder) {
+//         DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+//         authenticationProvider.setUserDetailsService(userService);
+//         authenticationProvider.setPasswordEncoder(passwordEncoder);
+//
+//         return new ProviderManager(authenticationProvider);
+//      }
 
+
+    }
 
 
 }
