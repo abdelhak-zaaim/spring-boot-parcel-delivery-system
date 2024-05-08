aa376be7e8f68875c249361724de5eef4cbe55d0
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-05-08 21:13:00 +0100

    edite AAdmin Entity , code refactor

--- src/main/java/com/suivi/colis/delix/entity/Admin.java
+++ src/main/java/com/suivi/colis/delix/entity/Admin.java
@@ -14,13 +14,20 @@
 package com.suivi.colis.delix.entity;
 
 import com.suivi.colis.delix.model.enums.Role;
+import com.suivi.colis.delix.model.enums.UserStatus;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.proxy.HibernateProxy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Objects;
 
 @NoArgsConstructor
@@ -28,9 +35,54 @@ import java.util.Objects;
 @Entity
 @Data
 @DiscriminatorValue(Role.ADMIN_ROLE)
-public class Admin extends Employee {
+public class Admin extends Employee implements UserDetails {
+    private final static String ROLE_PREFIX = "ROLE_";
+    private String adminEmployeeNumber;
+    @ManyToOne
+    private PrivilegesGroup privilegesGroup;
 
-private String adminEmployeeNumber;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(ROLE_PREFIX + this.getRole()));
 
 
+        if (this.privilegesGroup != null) {
+            this.privilegesGroup.getPrivileges().forEach(privilege -> {
+                authorities.add(new SimpleGrantedAuthority(privilege.name()));
+
+            });
+        }
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return super.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return !this.getStatus().equals(UserStatus.EXPIRED);
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !this.getStatus().equals(UserStatus.LOCKED);
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.getStatus() != null && !this.getStatus().equals(UserStatus.DISABLED);
+    }
 }
