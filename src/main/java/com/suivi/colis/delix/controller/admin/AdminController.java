02e01b5723ba5224aeae2f5f9f0c6d103caecb95
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-05-08 21:14:56 +0100

    code refactor

--- src/main/java/com/suivi/colis/delix/controller/admin/AdminController.java
+++ src/main/java/com/suivi/colis/delix/controller/admin/AdminController.java
@@ -11,13 +11,21 @@
 
 package com.suivi.colis.delix.controller.admin;
 
+import com.suivi.colis.delix.entity.User;
 import com.suivi.colis.delix.util.Constants;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class AdminController {
+
+  
+
+
 
     @GetMapping({"/admin/", "/admin"})
     public String admin(Model model) {
