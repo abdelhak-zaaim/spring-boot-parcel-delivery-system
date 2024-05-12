151dce4f3eb395f0c30eb2c8e92ca386e0a61539
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-05-12 23:34:24 +0100

    User Interface Inhaancement

---
+++ src/main/resources/static/home/assets/delixAnimated/css.css
@@ -0,0 +1,10 @@
+/*
+ *
+ *  * @project : DeliX
+ *  * @created : 12/05/2024, 18:48
+ *  * @modified : 12/05/2024, 18:48
+ *  * @description : This file is part of the DeliX project.
+ *  * @license : MIT License
+ *
+ */
+
