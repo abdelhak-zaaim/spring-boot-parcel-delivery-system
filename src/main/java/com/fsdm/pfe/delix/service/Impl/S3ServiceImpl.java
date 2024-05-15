c1e803da3e6b9e36855cb6d0084228540ae0f1a3
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-05-15 01:49:49 +0100

    enhancement , bug fixing

---
+++ src/main/java/com/fsdm/pfe/delix/service/Impl/S3ServiceImpl.java
@@ -0,0 +1,59 @@
+
+/*
+ *
+ *  * @project : DeliX
+ *  * @created : 14/05/2024, 21:49
+ *  * @modified : 14/05/2024, 21:49
+ *  * @description : This file is part of the DeliX project.
+ *  * @license : MIT License
+ *
+ */
+
+package com.fsdm.pfe.delix.service.Impl;
+
+import com.fsdm.pfe.delix.util.Constants;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.core.ResponseInputStream;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectResponse;
+
+import java.io.File;
+import java.nio.file.Paths;
+
+@Service
+public class S3ServiceImpl {
+
+    private final S3Client s3Client;
+
+    public S3ServiceImpl() {
+        AwsBasicCredentials awsCreds = AwsBasicCredentials.create(Constants.AWS_ACCESS, Constants.AWS_SECRET);
+        this.s3Client = S3Client.builder()
+                .region(Region.US_EAST_1)
+                .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
+                .build();
+    }
+
+    public PutObjectResponse uploadDocument(String bucketName, String key, String documentPath) {
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(key)
+                .build();
+
+        return s3Client.putObject(putObjectRequest, Paths.get(documentPath));
+
+    }
+
+    public void deleteDocument(String bucketName, String key) {
+        s3Client.deleteObject(builder -> builder.bucket(bucketName).key(key));
+    }
+
+
+
+
+}
