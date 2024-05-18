15765204093b000b7bd53308cf845d6e8093d6f5
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-05-18 22:59:05 +0100

    fixing ui issues

--- src/main/java/com/fsdm/pfe/delix/config/AdminCreationConfig.java
+++ src/main/java/com/fsdm/pfe/delix/config/AdminCreationConfig.java
@@ -43,7 +43,7 @@ public class AdminCreationConfig {
 public ApplicationRunner createAdminRunner() {
     return args -> {
         String adminEmail = "admin@admin.com";
-        String adminPassword = "admin1234";
+        String adminPassword = "12345678";
 
         // Check if the admin user already exists
         if (adminService.loadUserByEmail(adminEmail)== null) {
