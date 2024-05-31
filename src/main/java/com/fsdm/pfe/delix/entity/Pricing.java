bbfe98e43a9ab944a5e51104c22c8e9e71fd25b8
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-05-31 23:09:46 +0100

    Add detailed documentation to Pricing class

--- src/main/java/com/fsdm/pfe/delix/entity/Pricing.java
+++ src/main/java/com/fsdm/pfe/delix/entity/Pricing.java
@@ -25,15 +25,38 @@ import java.io.Serializable;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+/**
+ * This class represents the pricing model of the application
+ * It contains the base price, the density factor, the distance factor and the insurance cost factor
+ * The price of a delivery is calculated as follows:
+ * price = basePrice + (densityFactor * weight) + (distanceFactor * distance) + (insuranceCostFactor * insuranceCost)
+ * where weight is the weight of the delivery, distance is the distance between the sender and the receiver and insuranceCost is the cost of the insurance
+ * The default values are:
+ * basePrice = 25.0
+ * densityFactor = 0.5
+ * distanceFactor = 1.0
+ * insuranceCostFactor = 0.2
+ * The admin can change these values
+ * @see com.fsdm.pfe.delix.service.PricingService
+ * @see com.fsdm.pfe.delix.controller.PricingController
+ * @see com.fsdm.pfe.delix.service.Impl.PricingServiceImpl
+ * @see com.fsdm.pfe.delix.controller.Impl.PricingControllerImpl
+ * @see com.fsdm.pfe.delix.entity.Delivery
+
+ */
 public class Pricing implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    // this is the base price of a delivery en Mad
     private double basePrice = 25.0;
+
     private double densityFactor = 0.5;
+    // this is the distance factor
     private double distanceFactor = 1.0;
+    // this is the insurance cost factor
     private double insuranceCostFactor = 0.2;
+
 
 }
