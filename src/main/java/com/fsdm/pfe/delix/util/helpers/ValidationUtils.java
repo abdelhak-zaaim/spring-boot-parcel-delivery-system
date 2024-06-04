3f540f7fce58abf9b0c02a4c9e962095331a9420
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-06-04 16:57:09 +0100

    Refactor ValidationUtils for readability and simplicity and add methods descriptions

--- src/main/java/com/fsdm/pfe/delix/util/helpers/ValidationUtils.java
+++ src/main/java/com/fsdm/pfe/delix/util/helpers/ValidationUtils.java
@@ -13,27 +13,52 @@ package com.fsdm.pfe.delix.util.helpers;
 import java.util.regex.Pattern;
 
 public class ValidationUtils {
+    /**
+     * Check if email is valid
+     *
+     * @param email
+     * @return true if email is valid
+     */
     public static boolean isValidEmail(String email) {
         String emailRegex = "^[A-Za-z0-9+_.-]+@(.+)$";
         Pattern pat = Pattern.compile(emailRegex);
         if (email == null)
             return false;
+
         return pat.matcher(email).matches();
     }
 
+    /**
+     * Check if password is accepted
+     * @param password
+     * @return true if password is accepted
+     */
     public static boolean isAcceptedPassword(String password) {
+
         return isValidPassword(password) && !isEasyPassword(password);
     }
 
+    /**
+     * Check if password is valid
+     * @param password
+     * @return true if password is valid
+     */
     public static boolean isValidPassword(String password) {
+
         return password != null && password.length() >= 8 && !(password.length() > 50);
     }
 
+    /**
+     * Check if password is easy
+     * @param password
+     * @return true if password is easy
+     */
     public static boolean isEasyPassword(String password) {
         String passwordRegex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$";
         Pattern pat = Pattern.compile(passwordRegex);
         if (password == null)
             return false;
+
         return !pat.matcher(password).matches();
     }
 }
