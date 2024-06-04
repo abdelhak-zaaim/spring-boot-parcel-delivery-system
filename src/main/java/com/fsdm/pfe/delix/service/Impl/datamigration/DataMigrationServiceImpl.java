7153c592630162c8dafb116f4ec8c6444f4ad24e
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-06-04 20:03:12 +0100

    fix bug

--- src/main/java/com/fsdm/pfe/delix/service/Impl/datamigration/DataMigrationServiceImpl.java
+++ src/main/java/com/fsdm/pfe/delix/service/Impl/datamigration/DataMigrationServiceImpl.java
@@ -14,6 +14,7 @@ import com.fsdm.pfe.delix.service.datamegration.DataMigrationService;
 import com.fsdm.pfe.delix.service.datamegration.DataProcessor;
 import com.fsdm.pfe.delix.service.datamegration.DataReader;
 import com.fsdm.pfe.delix.service.datamegration.DataWriter;
+import com.opencsv.exceptions.CsvException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.MultipartFile;
@@ -52,6 +53,8 @@ public class DataMigrationServiceImpl implements DataMigrationService {
             dataWriter.writeDataToDatabase(processedData);
         } catch (IOException e) {
             LOGGER.error("Failed to read data from file. Error: {}", e.getMessage());
+        } catch (CsvException e) {
+            throw new RuntimeException(e);
         }
     }
 }
