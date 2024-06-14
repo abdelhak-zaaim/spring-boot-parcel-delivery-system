33862bfdf3b0ffcd48d53e7ed1341faf98e79501
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-06-14 17:41:50 +0100

    delete some comments

--- src/main/java/com/fsdm/pfe/delix/entity/Address.java
+++ src/main/java/com/fsdm/pfe/delix/entity/Address.java
@@ -37,16 +37,7 @@ import lombok.*;
 import javax.validation.constraints.NotNull;
 import java.util.Date;
 
-/**
- * This class represents an Address in the system.
- * It is an entity class that maps to a corresponding database table.
- *
- * @Entity annotation indicates that this class is a JPA entity.
- * @Getter, @Setter, @ToString, @RequiredArgsConstructor, and @AllArgsConstructor are Lombok annotations to reduce boilerplate code.
- *
- * @see jakarta.persistence.Entity
- * @see lombok
- */
+
 @Getter
 @Setter
 @ToString
@@ -55,48 +46,25 @@ import java.util.Date;
 @AllArgsConstructor
 public class Address {
 
-    /**
-     * The area of the address.
-     * @ManyToOne annotation indicates that this field should be mapped to a foreign key column in the database.
-     */
     @ManyToOne
     Area area;
 
-    /**
-     * The primary key of the Address.
-     * @Id annotation specifies the primary key of the entity.
-     * @GeneratedValue annotation provides the generation strategy specification for the primary key. GenerationType.IDENTITY indicates that the persistence provider must assign primary keys for the entity using a database identity column.
-     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    /**
-     * The address string.
-     * @Column annotation specifies that this field should be mapped to a column in the database. The 'nullable = false' attribute ensures that the column will not have null values.
-     */
     @Column(nullable = false)
     @NotNull
     private String address;
 
-    /**
-     * The creation date of the address.
-     * @Temporal annotation specifies the type of a temporal field.
-     */
     @Temporal(TemporalType.TIMESTAMP)
     @Column(updatable = false)
     private Date creationDate;
 
-    /**
-     * The last update date of the address.
-     * @Temporal annotation specifies the type of a temporal field.
-     */
     @Temporal(TemporalType.TIMESTAMP)
     private Date lastUpdateDate;
 
-    /**
-     * Method executed before the entity is persisted. Sets the creation and last update dates.
-     */
+
     @PrePersist
     protected void onCreated() {
         Date date = new Date();
@@ -104,18 +72,11 @@ public class Address {
         this.lastUpdateDate = date;
     }
 
-    /**
-     * Method executed before the entity is updated. Sets the last update date.
-     */
     @PreUpdate
     protected void onUpdate() {
         this.lastUpdateDate = new Date();
     }
 
-    /**
-     * Converts this Address entity to a AddressResponseDto.
-     * @return a AddressResponseDto representing this Address.
-     */
     public AddressResponseDto toAddressResponseDto() {
         return new AddressResponseDto(this);
     }
