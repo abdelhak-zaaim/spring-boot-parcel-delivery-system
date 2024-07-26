07f0cc6e55c8952c8c16654f786fa6401970fe8f
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-07-26 18:35:55 +0100

    update

--- src/main/java/com/fsdm/pfe/delix/entity/Area.java
+++ src/main/java/com/fsdm/pfe/delix/entity/Area.java
@@ -12,13 +12,16 @@ package com.fsdm.pfe.delix.entity;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
 
 import java.util.Date;
+import java.util.Objects;
 
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
@@ -40,6 +43,7 @@ public class Area {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "city_code", referencedColumnName = "code")
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @ToString.Exclude
     private City cityCode;
 
     private String postCode;
@@ -55,5 +59,20 @@ public class Area {
         this.updatedAt = new Date();
     }
 
+    @Override
+    public final boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null) return false;
+        Class<?> oEffectiveClass = object instanceof HibernateProxy ? ((HibernateProxy) object).getHibernateLazyInitializer().getPersistentClass() : object.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Area area = (Area) object;
+        return getId() != null && Objects.equals(getId(), area.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
 
